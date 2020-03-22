8c95318eea86ae0c471ec95942285672dcb2bb1c
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-03-22 15:25:01 -0500

    Cuerpo de http (body) en peticiones POST

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // importamos el modulo express | al ejecutar express esto me devuelve un objeto que es el servidor
 const express = require("express");
+const bodyParser = require("body-parser"); //importamos el modulo body parser para poder leer el body de las peticiones
 const app = express();
+
+// para poder leer lo que va en el body de las peticiones de los formularios | application/x-www-form-urlencoded usamos body parser o express.json para leer los json
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json());
+
 
 // Procesa parametros del "Query String" (son parametros que se pasan a la URL) /?nombre=willy & => para separar los parametros
 app.get("/", (req, res) => {
@@ -19,6 +25,15 @@ app.get("/usuario/:usuario/:cedula", (req, res) => {
 app.get("/autores/:autor", (req, res) => {
   res.send(`Autor ${req.params.autor}, año: ${req.query.año || "no definido como 'Query String' " }`);
 });
+
+// ruta que vamos a recibir lo que nos manda el cliente por el metodo POST
+app.post("/responder_encuesta", (req, res) => {
+    console.log(req.body) // veo lo que me envia en el body de la peticion
+
+    res.send("Respuesta del server!")
+})
+
+
 
 // servidor o app quiero que escuches en el puerto 3000 y cuando inicies ejecuta el mensaje por consola
 app.listen(3000, () => {
